be50d8620bed665cca564e26aeec7f0030218c0f
venky3000 <150220187+venky3000@users.noreply.github.com>
2025-08-03 14:31:07 +0000

    lecture 5

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version i.e. to declare that one's writing the code using new syntax of JS
+
+// alert("hello") works in browser console and not in node js
+
+// code readability should be high
+
+console.log(3 
+    +
+    3
+) // not readable
+
+console.log("Venky"); // readable
+
+
+
+let name = "Venky"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* 
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => no value assigned
+symbol => unique
+
+
+object
+*/
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
