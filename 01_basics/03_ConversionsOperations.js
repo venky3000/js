3e15e2f6fbff980d5d966114ebb0d691cb5ff3f6
venky3000 <150220187+venky3000@users.noreply.github.com>
2025-08-05 15:25:37 +0000

    conversions and operations

---
+++ 01_basics/03_ConversionsOperations.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+/*console.log(typeof score);
+console.log(typeof(score));*/
+
+let valueInNumber = Number(score)
+
+/*console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+
+/*
+"33" -> 33
+"33abc" -> NaN
+true, false -> 1, 0
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+/*
+1, 0 -> true, false
+"" -> false
+"Venky" -> true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
