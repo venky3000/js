11e5ffd867e76f3e584514ebe33547c895861b25
venky3000 <150220187+venky3000@users.noreply.github.com>
2025-08-06 12:37:29 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,56 @@
+/* 
+Primitive
+7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+*/
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 3425342534232356456345n
+
+/*
+Reference(Non Primitive)
+
+Arrays, Objects, Functions
+*/
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Venky",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof score);
+
+console.log(typeof scoreValue);
+
+console.log(typeof isLoggedIn);
+
+console.log(typeof outsideTemp);
+
+console.log(typeof userEmail);
+
+console.log(typeof id);
+
+console.log(typeof anotherId);
+
+console.log(typeof bigNumber);
+
+console.log(typeof heros);
+
+console.log(typeof myObj);
+
+console.log(typeof myFunction);
