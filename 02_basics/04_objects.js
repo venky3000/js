e328b230de31f52b3e88640a8dad83ec7befeaf1
venky3000 <150220187+venky3000@users.noreply.github.com>
2025-08-16 15:55:32 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -45,10 +45,36 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursername: "js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course // object destructuring
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "venky",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// {
+//     {},
+//     {},
+//     {}
+// }
