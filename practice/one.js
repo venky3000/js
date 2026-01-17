08c704de25dbabd14acdc1c03a1059f6a1b6a5e6
venky3000 <150220187+venky3000@users.noreply.github.com>
2026-01-17 16:01:28 +0000

    Practice

---
+++ practice/one.js
@@ -0,0 +1,5 @@
+console.log("5" + 3); // 53 correct
+console.log("5" - 3); // 2 correct
+console.log(true + 1); // true wrong
+console.log(false == 0); // false wrong
+console.log(null === undefined); // false correct
